2800c6e834c3bb6ca11e65a628eaa8edde6f5a56
Sakshi Pandey <sakshirampravesh1@gmail.com>
2024-04-25 17:18:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-/*import { PaginationTable } from "./components/PaginationTable"; */
-
-import { SortingTable } from "./components/SortingTable" 
 import PaginationTable from "./components/PaginationTable";
-/*import FilteringTable from "./components/FilteringTable";
-/*import { FilteringTable } from "./components/FilteringTable"  */
+
 function App() {
-  return(
-    <div className = "App">
-      <FilteringTable />
+  return (
+    <div className="App">
+      <PaginationTable />
     </div>
   );
 }
--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -1,13 +1,13 @@
-import React, { useMemo } from 'react'
-import { useTable, usePagination} from 'react-table'
-import MOCK_DATA from './MOCK_DATA.json'
-import { COLUMNS , GROUPED_COLUMNS} from './columns'
-import './table.css'
-import { CSVLink, CSVDownload } from "react-csv";
+import React, { useMemo } from "react";
+import { useTable, usePagination } from "react-table";
+import { COLUMNS } from "./columns";
+import "./table.css";
+import { CSVLink } from "react-csv";
+import mockData from "./MOCK_DATA.json";
 
-export const PaginationTable = () => {
-  const columns = useMemo(() => COLUMNS, [])
-  const data = useMemo(() => MOCK_DATA, [])
+const PaginationTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => mockData, []);
 
   const {
     getTableProps,
@@ -15,9 +15,9 @@ export const PaginationTable = () => {
     headerGroups,
     page,
     nextPage,
-    previousPage, /*helper function*/
+    previousPage /*helper function*/,
     canNextPage,
-    canPreviousPage,  /*boolean properties that indicate whether you can go next or previous */
+    canPreviousPage /*boolean properties that indicate whether you can go next or previous */,
     pageOptions,
     gotoPage,
     pageCount,
@@ -25,63 +25,63 @@ export const PaginationTable = () => {
     prepareRow,
   } = useTable(
     {
-    columns,
-    data,
+      columns,
+      data,
     },
     usePagination
-)
+  );
 
-  const { pageIndex } = state
-  const MOCK_DATA = [
-    ["firstname", "lastname", "email"],
-    ["Ahmed", "Tomi", "ah@smthing.co.com"],
-    ["Raed", "Labes", "rl@smthing.co.com"],
-    ["Yezzi", "Min l3b", "ymin@cocococo.com"]
-  ];
+  const { pageIndex } = state;
+
   return (
     <>
-    <CSVLink data={csvData}>Download me</CSVLink>
+      <CSVLink data={mockData}>Download me</CSVLink>
       <table {...getTableProps()}>
         <thead>
-          {headerGroups.map(headerGroup => (
+          {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {page.map(row => {
-            prepareRow(row)
+          {page.map((row) => {
+            prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
                 })}
               </tr>
-            )
+            );
           })}
         </tbody>
-        
       </table>
       <div>
         <span>
-            Page{' '}
-            <strong>
-                {pageIndex + 1} of {pageOptions.length}
-            </strong>{' '}
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{" "}
         </span>
         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-            {'<<'}
+          {"<<"}
         </button>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>Previous</button>
-        <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
         <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-            {'>>'}
-        </button>      
+          {">>"}
+        </button>
       </div>
     </>
-  )
-}
+  );
+};
 export default PaginationTable;
